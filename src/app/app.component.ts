ff6f3c0569305ee9cfdc8a7265abc40ea4a78d7f
jannamryan1997 <jannamryan8@gmail.com>
2020-05-26 12:02:21 +0400

    changed bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,11 @@ import { CookieService } from 'ngx-cookie-service';
 export class AppComponent {
   title = 'marathon';
 
-  constructor(private _cookieService: CookieService){}
+  constructor(private _router: Router) { }
+
+  ngOnInit() {
+    this._router.events.subscribe((evt) => {
+      window.scrollTo(0, 0)
+    });
+  }
 }
