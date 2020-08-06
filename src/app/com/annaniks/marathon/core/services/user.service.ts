4fdf6c46eca2b7de87ec4cc9f3ce526b25caffcc
jannamryan1997 <jannamryan8@gmail.com>
2020-08-06 09:42:04 +0400

    created upload_file

--- src/app/com/annaniks/marathon/core/services/user.service.ts
+++ src/app/com/annaniks/marathon/core/services/user.service.ts
@@ -43,3 +43,4 @@ export class UserService {
 }
 
 
+// http://192.168.1.115:8000/api
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiUrl: "http://annaniks.com:6262/api",
   //fileUrl: "http://annaniks.com:6262/media/",
-  // apiUrl:"http://192.168.1.115:8000/api"  local
+  // apiUrl:"http://192.168.1.115:8000/api"  
 
   apiUrl: "https://uat.marathon.me/api",
   fileUrl: "https://uat.marathon.me/media/",
