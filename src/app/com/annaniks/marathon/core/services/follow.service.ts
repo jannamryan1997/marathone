6f22403138a8f806c87004cf1c8b9bbcf62655c7
Armenuhi1997 <armenuhimanukyan@annaniks.com>
2020-08-10 11:05:54 +0400

    ixed bugs

--- src/app/com/annaniks/marathon/core/services/follow.service.ts
+++ src/app/com/annaniks/marathon/core/services/follow.service.ts
@@ -9,21 +9,25 @@ export class FollowService {
     private _isFollowed = false;
     public followEvent$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
     constructor(private _profileService: ProfileService) { }
-    public getAuthState(): Observable<boolean> {
+
+    public getFollowState(): Observable<boolean> {
         return this.followEvent$.asObservable();
+    }
+    public changeFollowStateValue(value: boolean) {
+        this.followEvent$.next(value);
     }
     public follow(user, role, myUrl, userRole, userUrl) {
         if (!user.is_follower) {
             return this._profileService.follow(role, myUrl, userRole, userUrl).pipe(
                 map(() => {
-                    this.followEvent$.next(true);
+                    this.changeFollowStateValue(true);
                     return true;
                 }))
         } else {
             if (user.is_follower_id) {
                 return this._profileService.unfollow(user.is_follower_id).pipe(
                     map(() => {
-                        this.followEvent$.next(true);
+                        this.changeFollowStateValue(true);
                         return true;
                     }))
             }
--- src/app/com/annaniks/marathon/layout/user-item/user-item.component.ts
+++ src/app/com/annaniks/marathon/layout/user-item/user-item.component.ts
@@ -14,45 +14,44 @@ import { FollowService } from '../../core/services/follow.service';
 })
 export class UserItemComponent {
     private unsubscribe$ = new Subject<void>()
-    public item: Familiar;
+    public items: Familiar[] = [];
     private _role: string;
     private _defaultImage: string = '/assets/images/user-icon-image.png'
     @Input('item')
-    set setItem($event: Familiar) {
-        this.item = $event
+    set setItem($event: Familiar[]) {
+        this.items = $event
     }
 
     @Output('getUser') private _getUser = new EventEmitter()
     @Input('type') public type: string;
     constructor(@Inject('FILE_URL') public fileURL,
         private _followService: FollowService,
-        private _profileService: ProfileService,
         private _cookieService: CookieService,
         private _userService: UserService) {
         this._role = this._cookieService.get('role');
-
     }
+
     ngOnInit() {
-        this._followService.followEvent$.pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
+        this._followService.getFollowState().pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
             if (data) {
-                this._followService.followEvent$.next(false)
-                this._getUser.emit(true)
+                this._followService.changeFollowStateValue(false);
+                this._getUser.emit(true);
             }
         })
     }
-    public follow() {
-        this._followService.follow(this.item, this._role, this._userService.user.data.url, this.item.role, this.item.url)
+    public follow(item: Familiar) {
+        this._followService.follow(item, this._role, this._userService.user.data.url, item.role, item.url)
             .pipe(takeUntil(this.unsubscribe$)).subscribe()
     }
-    public getAvatar() {
+    public getAvatar(item: Familiar) {
 
-        if (this.item && this.item.avatar) {
-            return this.fileURL + this.item.avatar
+        if (item && item.avatar) {
+            return this.fileURL + item.avatar
         } else {
             return this._defaultImage
         }
     }
-    public getLinkUrl() {
-        return `/profile/${this.item.slug}/${this.item.role}`
+    public getLinkUrl(item: Familiar) {
+        return `/profile/${item.slug}/${item.role}`
     }
 }
