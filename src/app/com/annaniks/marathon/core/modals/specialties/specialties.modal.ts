f7e4641808442134e08d4e2b02c4d3d251030c4e
jannamryan1997 <jannamryan1997>
2020-08-17 22:51:28 -0700

    created speciality item

--- src/app/com/annaniks/marathon/core/modals/specialties/specialties.modal.ts
+++ src/app/com/annaniks/marathon/core/modals/specialties/specialties.modal.ts
@@ -1,44 +1,72 @@
 import { Component, OnInit, OnDestroy, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+
 @Component({
-    selector: "app-specialties",
-    templateUrl: "specialties.modal.html",
-    styleUrls: ["specialties.modal.scss"]
+  selector: "app-specialties",
+  templateUrl: "specialties.modal.html",
+  styleUrls: ["specialties.modal.scss"]
 })
 
 export class SpecialtiesModal implements OnInit, OnDestroy {
-   public  selectable: boolean = true;
- public  selectedOffers: any[] = [];
- public speciality;
-    constructor(@Inject(MAT_DIALOG_DATA) private _data,private _dialogRef: MatDialogRef<SpecialtiesModal>) {
-        this.speciality=this._data.data;
-     }
+  public selectable: boolean = true;
+  public selectedOffers: any[] = [];
+  public speciality;
+  public activeItem;
+  public selectedItem;
+  public matChip;
+  public a = 1;
+  constructor(@Inject(MAT_DIALOG_DATA) private _data, private _dialogRef: MatDialogRef<SpecialtiesModal>) {
+    this.speciality = this._data.data;
+    this.activeItem = this._data.activeItem;
+  }
 
-    public closeModal(): void {
-        this._dialogRef.close();
-    }
 
-    ngOnInit() { }
+  ngOnInit() {
+    console.log(this.speciality)
+    if (this.activeItem) {
+      for (let item of this.activeItem) {
+        this.selectedItem = item;
+        for (let speciality of this.speciality) {
+          if (item === speciality) {
+            console.log('ddd');
 
-    isSelected(offer: any): boolean {    
-        const index = this.selectedOffers.indexOf(offer);
-        return index >= 0;
-      }
+            this.selectedOffers.push(this.selectedItem);
+            console.log(this.selectedOffers);
 
-      toggleOffer(offer: any): void {
-        let index = this.selectedOffers.indexOf(offer);
-    
-        if (index >= 0) {
-          this.selectedOffers.splice(index, 1);
-        } else {
-          this.selectedOffers.push(offer);
+          }
         }
       }
+    }
+  }
+
+  public closeModal(): void {
+    this._dialogRef.close();
+  }
+
+  isSelected(offer: any): boolean {
+    const index = this.selectedOffers.indexOf(offer);
+    return index >= 0;
+  }
+
+  toggleOffer(offer: any): void {
+    let index = this.selectedOffers.indexOf(offer);
+    console.log(index);
     
 
-      public sentselectedOffers():void{
-          this._dialogRef.close(this.selectedOffers);
-      }
-    ngOnDestroy() { }
+    if (index >= 0) {
+      this.selectedOffers.splice(index, 1);
+    } else {
+      this.selectedOffers.push(offer);
+    }
+  }
+
+
+  public sentselectedOffers(): void {
+    this._dialogRef.close(this.selectedOffers);
+
+  }
+
+
+  ngOnDestroy() { }
 }
--- src/app/com/annaniks/marathon/pages/main/edit-profile/edit-profile.view.ts
+++ src/app/com/annaniks/marathon/pages/main/edit-profile/edit-profile.view.ts
@@ -12,7 +12,7 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { SpecialtiesModal } from '../../../core/modals';
 import { MatDialog } from '@angular/material/dialog';
-import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import { MatAutocompleteSelectedEvent, MatAutocomplete } from '@angular/material/autocomplete';
 import { Router, ActivatedRoute } from '@angular/router';
 @Component({
     selector: "app-edit-profile",
@@ -42,10 +42,10 @@ export class EditProfileView implements OnInit {
     public spacialityUrl = [];
     public countries = [];
     public educationItem: EducationData[] = [];
-    public experienceItem:ExperienceData[]=[];
-    public routerUrl:string;
-    public _unsbscribe=new Subject<void>();
-    public maleTab:string="male";
+    public experienceItem: ExperienceData[] = [];
+    public routerUrl: string;
+    public _unsbscribe = new Subject<void>();
+    public maleTab: string = "male";
     @ViewChild('fruitInput') fruitInput: ElementRef<HTMLInputElement>;
     @ViewChild('auto') matAutocomplete: MatAutocomplete;
     constructor(
@@ -55,15 +55,15 @@ export class EditProfileView implements OnInit {
         private _userService: UserService,
         private _profileService: ProfileService,
         private _location: Location,
-        private _dialog:MatDialog,
-        private _router:Router,
-        private _activatedRouter:ActivatedRoute,
+        private _dialog: MatDialog,
+        private _router: Router,
+        private _activatedRouter: ActivatedRoute,
 
         @Inject("FILE_URL") public _fileUrl: string,
     ) {
         this.role = this._cookieService.get('role');
         this.user = this._userService.user;
-        
+
 
         if (this._userService.user.data.avatar) {
             this.localImage = this._fileUrl + this._userService.user.data.avatar;
@@ -93,13 +93,13 @@ export class EditProfileView implements OnInit {
             instagram: [null],
             linkedin: [null],
             youtube: [null],
-            user_name:[null],
+            user_name: [null],
         })
     }
 
     private _setPatchValue(): void {
-        
-        this.profileFormGroup.patchValue({    
+
+        this.profileFormGroup.patchValue({
             firstName: this.user.data.user.first_name,
             userName: this.user.data.user.last_name,
             status: this.user.data.status,
@@ -108,9 +108,9 @@ export class EditProfileView implements OnInit {
             instagram: this.user.data.instagram,
             linkedin: this.user.data.linkedin,
             location: this.user.data.location,
-            user_name:this.user.data.slug,
+            user_name: this.user.data.slug,
         });
-console.log( this.user.data.location);
+        console.log(this.user.data.location);
 
 
         this.educationItem = [];
@@ -210,7 +210,7 @@ console.log( this.user.data.location);
         }
         if (this.role === 'client') {
             this._userService.putClient(this._userService.user.data.id, this._userService.user.data)
-            .pipe(takeUntil(this._unsbscribe))
+                .pipe(takeUntil(this._unsbscribe))
                 .subscribe((data) => {
                     this._userService.getClient().subscribe((data) => {
                         if (type === 'avatar') {
@@ -229,7 +229,7 @@ console.log( this.user.data.location);
         }
         else {
             this._userService.putCoatch(this._userService.user.data.id, this._userService.user.data)
-            .pipe(takeUntil(this._unsbscribe))
+                .pipe(takeUntil(this._unsbscribe))
                 .subscribe((data) => {
                     this._userService.getCoatch().subscribe((data) => {
                         if (type === 'avatar') {
@@ -247,42 +247,42 @@ console.log( this.user.data.location);
     }
     private _getCountries(): void {
         this._countryService.getLanguages()
-        .pipe(takeUntil(this._unsbscribe))
-        .subscribe((countries) => {
-            this.countries = countries.results;
-            const selectedlanguages: any[] = this.user.data.language || [];
-            const selectedcountries = [];
-            selectedlanguages.map((e) => {
-                const findedCountry = this.countries.find((f) => f.url === e);
-                if (findedCountry) {
-                    selectedcountries.push(findedCountry);
-                }
-            })
-            this.profileFormGroup.patchValue({
-                languages: selectedcountries
-            })
-        });
+            .pipe(takeUntil(this._unsbscribe))
+            .subscribe((countries) => {
+                this.countries = countries.results;
+                const selectedlanguages: any[] = this.user.data.language || [];
+                const selectedcountries = [];
+                selectedlanguages.map((e) => {
+                    const findedCountry = this.countries.find((f) => f.url === e);
+                    if (findedCountry) {
+                        selectedcountries.push(findedCountry);
+                    }
+                })
+                this.profileFormGroup.patchValue({
+                    languages: selectedcountries
+                })
+            });
     }
 
     private _getSpeciality(): void {
         this._countryService.getSpeciality()
-        .pipe(takeUntil(this._unsbscribe))
-        .subscribe((speciality) => {
-            this.speciality = speciality.results;
-            const selectedspaciality: any[] = this.user.data.speciality || [];
-            const selectspaciality = [];
-            selectedspaciality.map((e) => {
-                const findedCountry = this.speciality.find((f) => f.url === e);
-                if (findedCountry) {
-                    selectspaciality.push(findedCountry);
-                }
-            })
-            this.profileFormGroup.patchValue({
-                speciality: selectspaciality
-            })
+            .pipe(takeUntil(this._unsbscribe))
+            .subscribe((speciality) => {
+                this.speciality = speciality.results;
+                const selectedspaciality: any[] = this.user.data.speciality || [];
+                const selectspaciality = [];
+                selectedspaciality.map((e) => {
+                    const findedCountry = this.speciality.find((f) => f.url === e);
+                    if (findedCountry) {
+                        selectspaciality.push(findedCountry);
+                    }
+                })
+                this.profileFormGroup.patchValue({
+                    speciality: selectspaciality
+                })
 
 
-        });
+            });
     }
 
     ///--------------PICTURE UPLOAD
@@ -312,16 +312,16 @@ console.log( this.user.data.location);
             // this.user.data.certificates = this.certificates,
             this.user.data.language = this.url,
             this.user.data.speciality = this.spacialityUrl;
-            this.user.data.slug=this.profileFormGroup.value.user_name,
-        this._userService.putCoatch(this._userService.user.data.id, this.user.data)
-        .pipe(takeUntil(this._unsbscribe))
-            .subscribe((data) => {
-                this._userService.getCoatch().subscribe((data) => {
-                    
-                    this.user.data = data.data;
-                    this._router.navigate([`/profile/${this.user.data.slug}/coach`]);
+        this.user.data.slug = this.profileFormGroup.value.user_name,
+            this._userService.putCoatch(this._userService.user.data.id, this.user.data)
+                .pipe(takeUntil(this._unsbscribe))
+                .subscribe((data) => {
+                    this._userService.getCoatch().subscribe((data) => {
+
+                        this.user.data = data.data;
+                        this._router.navigate([`/profile/${this.user.data.slug}/coach`]);
+                    })
                 })
-            })
 
     }
 
@@ -345,14 +345,14 @@ console.log( this.user.data.location);
             this.user.data.location = this.profileFormGroup.value.location,
             this.user.data.language = this.url,
             this.user.data.speciality = this.spacialityUrl;
-            this.user.data.slug=this.profileFormGroup.value.user_name,
-        this._userService.putClient(this._userService.user.data.id, this.user.data)
-        .pipe(takeUntil(this._unsbscribe))
-            .subscribe((data) => {
-                this._userService.getClient().subscribe((data) => {
-                    this._router.navigate([`/profile/${this.user.data.slug}/client`]);
+        this.user.data.slug = this.profileFormGroup.value.user_name,
+            this._userService.putClient(this._userService.user.data.id, this.user.data)
+                .pipe(takeUntil(this._unsbscribe))
+                .subscribe((data) => {
+                    this._userService.getClient().subscribe((data) => {
+                        this._router.navigate([`/profile/${this.user.data.slug}/client`]);
+                    })
                 })
-            })
     }
 
     /////----------------------UPDATE USER AND COATC
@@ -376,7 +376,7 @@ console.log( this.user.data.location);
     public removeEducationItem(event, ind, item): void {
         if (event && item.value.url) {
             this._profileService.deleteProfileInformation(item.value.url, item.value)
-            .pipe(takeUntil(this._unsbscribe))
+                .pipe(takeUntil(this._unsbscribe))
                 .subscribe((data) => {
                     if (this.role === 'client') {
                         this._getClient();
@@ -397,10 +397,10 @@ console.log( this.user.data.location);
         this.experience.push(new FormControl(''));
     }
 
-    public removeExperianceItem(event, ind,item): void {
+    public removeExperianceItem(event, ind, item): void {
         if (event && item.value.url) {
             this._profileService.deleteProfileInformation(item.value.url, item.value)
-            .pipe(takeUntil(this._unsbscribe))
+                .pipe(takeUntil(this._unsbscribe))
                 .subscribe((data) => {
                     if (this.role === 'client') {
                         this._getClient();
@@ -425,16 +425,16 @@ console.log( this.user.data.location);
 
     public addCertificates(): void {
         this.certificates.push({ file: this.certificatesImage, description: this.profileFormGroup.value.certificatesLocation });
-        this.certificatesImage='';
+        this.certificatesImage = '';
         this.profileFormGroup.patchValue({
-            certificatesLocation :'', 
+            certificatesLocation: '',
         });
-        }
+    }
 
     public removeCerticatesItem(event, ind, item): void {
         if (event && item.url) {
             this._profileService.deleteProfileInformation(item.url, item)
-            .pipe(takeUntil(this._unsbscribe))
+                .pipe(takeUntil(this._unsbscribe))
                 .subscribe((data) => {
                     if (this.role === 'client') {
                         this._getClient();
@@ -450,17 +450,17 @@ console.log( this.user.data.location);
         }
     }
     public onClickShowSocialMedium(): void {
-        if(this.showMore===true){
-            this.showMore=false;
-         
+        if (this.showMore === true) {
+            this.showMore = false;
+
         }
         this.showSocialMedium = !this.showSocialMedium;
-        
+
     }
     public onClickShowMore(): void {
-        if(this.showSocialMedium===true){
-            this.showSocialMedium=false;
-        
+        if (this.showSocialMedium === true) {
+            this.showSocialMedium = false;
+
         }
         this.showMore = !this.showMore;
     }
@@ -511,11 +511,11 @@ console.log( this.user.data.location);
         return filtered;
     }
 
-    public onClickedOutside(event):void{
-        this.showMore=false;
-        this.showSocialMedium=false;
+    public onClickedOutside(event): void {
+        this.showMore = false;
+        this.showSocialMedium = false;
     }
-    public copyUrl():void{
+    public copyUrl(): void {
         const selBox = document.createElement('textarea');
         selBox.style.position = 'fixed';
         selBox.style.left = '0';
@@ -529,42 +529,40 @@ console.log( this.user.data.location);
         document.body.removeChild(selBox);
     }
 
-    public onClickMaleTab(event):void{
-        this.maleTab=event;
+    public onClickMaleTab(event): void {
+        this.maleTab = event;
     }
-   
-public openSpecialtiesModal():void{
-    const dialogRef=this._dialog.open(SpecialtiesModal,{
-        width:" 520px",
-        data:{
-            data:this.speciality,
-        }
-    })
-    dialogRef.afterClosed().subscribe((data)=>{
-        if(data){
-            this.profileFormGroup.patchValue({
-                speciality:data,
-            })
-            
-        }
-   
-        
-    })
-}
-selected(event: MatAutocompleteSelectedEvent): void {
-    this.countries.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.profileFormGroup.value.languages.setValue(null);
-  }
+
+    public openSpecialtiesModal(): void {
+        let activeItem = this.profileFormGroup.value.speciality
+        const dialogRef = this._dialog.open(SpecialtiesModal, {
+            width: " 520px",
+            data: {
+                data: this.speciality,
+                activeItem: activeItem,
+            }
+        })
+        dialogRef.afterClosed().subscribe((data) => {
+            if (data) {
+                this.profileFormGroup.patchValue({
+                    speciality: data,
+                })
+
+            }
 
 
-public stopPropagation(event){
-    console.log("Dddddddd");
-    event.stopPropagation();
- 
-    
-    
-}
+        })
+    }
+    selected(event: MatAutocompleteSelectedEvent): void {
+        this.countries.push(event.option.viewValue);
+        this.fruitInput.nativeElement.value = '';
+        this.profileFormGroup.value.languages.setValue(null);
+    }
+
+
+    public stopPropagation(event) {
+        event.stopPropagation();
+    }
 
 
 }
