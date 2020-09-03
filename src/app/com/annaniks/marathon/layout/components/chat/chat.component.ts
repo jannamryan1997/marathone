0881631094913f5037bbfbf280d14e40ec96b0e0
Armenuhi1997 <armenuhimanukyan@annaniks.com>
2020-09-03 17:05:08 +0400

    set code in component

--- src/app/com/annaniks/marathon/layout/components/chat/chat.component.ts
+++ src/app/com/annaniks/marathon/layout/components/chat/chat.component.ts
@@ -1,5 +1,14 @@
-import { Component, Input, Inject, Output, EventEmitter, ViewChild, ElementRef, ViewChildren, QueryList } from "@angular/core";
+import { Component, Input, Inject, Output, EventEmitter} from "@angular/core";
 import { FormBuilder } from '@angular/forms';
+import { IUser, ITopic } from '../../../core/models/topic';
+import { Action } from '../../../core/services/generated/chat_pb';
+import { HttpResponse } from '@angular/common/http';
+import { TopicMessage, ITopicMessage } from '../../../core/models/topic-message';
+import { ChatService, TopicActions } from '../../../core/services/chat.service';
+import { JhiEventManager } from 'ng-jhipster';
+import { UserService } from '../../../core/services/user.service';
+import { CookieService } from 'ngx-cookie';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-chat',
@@ -9,9 +18,9 @@ import { FormBuilder } from '@angular/forms';
 export class ChatComponent {
     public item;
     private _defaultImage: string = '/assets/images/user-icon-image.png';
-    inputForm = this.fb.group({
-        text: [],
-    });
+    // inputForm = this.fb.group({
+    //     text: [],
+    // });
     @Output('closeItem') private _close = new EventEmitter()
 
     @Input('activeChat')
@@ -26,13 +35,183 @@ export class ChatComponent {
         },
         {
             role: 'my',
-            message: 'hi'
+            message: 'aaaa'
         }
     ]
-    constructor(@Inject('FILE_URL') public fileURL, private fb: FormBuilder) { }
+   
+    topics: ITopic[];
+    topicActions: TopicActions[];
+    topicMessages: Map<number, TopicMessage[]>;
+    topicSelected: ITopic | null;
+    sendingMessage: boolean;
+    eventSubscriber?: Subscription;
+    inputForm = this.fb.group({
+        text: [],
+    });
 
-    ngOnInit() { }
+    constructor(
+        protected chatService: ChatService,
+        protected eventManager: JhiEventManager,
+        private fb: FormBuilder,
+        private _userService: UserService,
+        private _cookieService: CookieService,
+        @Inject('FILE_URL') public fileURL
+    ) {
+        this.topicMessages = new Map<number, TopicMessage[]>();
+        this.sendingMessage = false;
+        this.topicActions = [];
+        this.chatService.topicActions.subscribe(value => {
+            this.topicActions = [];
+            value.forEach(ta => this.topicActions.push(ta));
+        });
+        this.chatService.topicMessages.subscribe(tm => {
+            const currentTopicMessages: TopicMessage[] = this.topicMessages[tm.topicId || 0] || [];
+            const exists = currentTopicMessages.some(cv => cv.id === tm.id);
+            if (!exists) {
+                currentTopicMessages.push(tm);
+                this.topicMessages[tm.topicId || 0] = currentTopicMessages;
+            }
+        });
+        this.chatService.sendingMessage.subscribe(v => (this.sendingMessage = v));
+        this.topics = [];
+        this.topicSelected = null;
+        this.inputForm.get('text')?.valueChanges?.subscribe(value => {
+            let text;
+            if (value) {
+                text = value.toString();
+            } else {
+                text = '';
+            }
+            this.chatService.sendTopicTextEvent(this.topicSelected, text.length > 0);
+        });
+    }
 
+    setSelected(topic: ITopic): void {
+        this.inputForm.patchValue({
+            text: '',
+        });
+
+        this.chatService.sendTopicEvent(topic);
+        this.topicSelected = topic;
+        const topicId = this.topicSelected.id;
+        if (topicId) {
+            this.chatService
+                .queryMessages(topicId)
+                .subscribe((res: HttpResponse<ITopicMessage[]>) => this.addTopicMessages(topicId, res.body || []));
+        }
+    }
+
+    addTopicMessages(topicId: number, messages: TopicMessage[]): void {
+        const currentTopicMessages: TopicMessage[] = this.topicMessages[topicId] || [];
+        for (let mi = 0; mi < messages.length; mi++) {
+            const message: TopicMessage = messages[mi];
+            let exists = false;
+            for (let me = 0; me < currentTopicMessages.length; me++) {
+                const messageExisting: TopicMessage = currentTopicMessages[me];
+                if (messageExisting.id === message.id) {
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                currentTopicMessages.push(message);
+            }
+        }
+        currentTopicMessages.sort((ma, mb) => {
+            if ((ma.createdAt || 0) > (mb.createdAt || 0)) {
+                return 1;
+            }
+            if ((ma.createdAt || 0) < (mb.createdAt || 0)) {
+                return -1;
+            }
+            return 0;
+        });
+        console.log(currentTopicMessages);
+        this.topicMessages[topicId] = currentTopicMessages;
+    }
+
+    loadAll(): void {
+        this.chatService.queryTopics().subscribe((res: HttpResponse<ITopic[]>) => {
+            if (res && res.body) {
+                this.chatService.updateTopics(res.body);
+                for (let i = 0; i < res?.body?.length; i++) {
+                    if (i === 0) {
+                        this.setSelected(res.body[i]);
+                    }
+                    this.topics.push(res.body[i]);
+                }
+            }
+        });
+    }
+
+    ngOnInit(): void {
+        // this.chatService.sendOptionsRequiest().subscribe((data) => {
+        this.loadAll();
+        // })
+    }
+
+    ngOnDestroy(): void {
+        if (this.eventSubscriber) {
+            this.eventManager.destroy(this.eventSubscriber);
+        }
+    }
+
+    formatTopicTitle(topic: ITopic): string {
+        const res = [];
+        if (topic.participants) {
+            for (let i = 0; i < (topic.participants.length || 0); i++) {
+                if (topic.participants[i].id !== this.getUserId()) {
+                    res.push(topic.participants[i].firstName + ' ' + topic.participants[i].lastName);
+                }
+            }
+        }
+        if (res.length === 0 && topic.title) {
+            res.push(topic.title);
+        }
+
+        return res.join(' , ');
+    }
+
+    getUserId(): number {
+        let chatId = +this._cookieService.get('chatId');
+        return chatId
+    }
+
+    isOnline(participant: IUser, topicSelected: ITopic): boolean {
+        if (!topicSelected.id || !participant.id) {
+            return false;
+        }
+        for (let index = 0; index < this.topicActions.length; index++) {
+            if (this.topicActions[index].id === topicSelected.id && this.topicActions[index].actions[participant.id]) {
+
+                return this.topicActions[index].actions[participant.id] >= Action.ACTION_ONLINE;
+            }
+        }
+        return false;
+    }
+
+    isWriting(participant: IUser, topicSelected: ITopic): boolean {
+        if (!topicSelected.id || !participant.id) {
+            return false;
+        }
+        for (let index = 0; index < this.topicActions.length; index++) {
+            if (this.topicActions[index].id === topicSelected.id && this.topicActions[index].actions[participant.id]) {
+                return this.topicActions[index].actions[participant.id] >= Action.ACTION_TOPIC_FULL;
+            }
+        }
+        return false;
+    }
+
+    sendText(): void {
+        if (this.sendingMessage) {
+            return;
+        }
+        this.chatService.sendTopicMessageEvent(this.topicSelected, this.inputForm.get('text')?.value).subscribe(v => {
+            if (v) {
+                this.inputForm.get('text')?.patchValue('');
+            }
+        });
+    }
     public getAvatar(item) {
         if (item && item.avatar) {
             // this.fileURL + 
