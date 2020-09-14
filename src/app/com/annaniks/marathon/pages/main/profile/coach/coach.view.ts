8d925050adfbfdf24ff199b2e5fa20aa4cca2ba5
Armenuhi97 <armenuhimanukyan@annaniks.com>
2020-09-14 15:27:58 +0400

    copy refferal link

--- src/app/com/annaniks/marathon/pages/main/profile/coach/coach.view.ts
+++ src/app/com/annaniks/marathon/pages/main/profile/coach/coach.view.ts
@@ -172,12 +172,13 @@ export class CoachView implements OnInit {
                 this.specialityName = []
                 data.results.map((name, index) => {
                     url = name.url;
-                    this._userService.user.data.speciality.forEach(element => {
-                        if (url === element) {
-                            this.specialityName.push({ name: name.name });
+                    if (this._userService.user && this._userService.user.data)
+                        this._userService.user.data.speciality.forEach(element => {
+                            if (url === element) {
+                                this.specialityName.push({ name: name.name });
 
-                        }
-                    })
+                            }
+                        })
                 })
             })
     }
@@ -214,8 +215,8 @@ export class CoachView implements OnInit {
         this.galerryTab = tab;
         if (this.galerryTab === 2) {
 
-            let imageContent = this.feedMediaItem.filter((data) => { return( data.feed_media && data.feed_media[0] && data.feed_media[0].content) ? (data.feed_media[0].content.type == 'image') : null });
-           
+            let imageContent = this.feedMediaItem.filter((data) => { return (data.feed_media && data.feed_media[0] && data.feed_media[0].content) ? (data.feed_media[0].content.type == 'image') : null });
+
             if (imageContent && imageContent.length) {
                 this.showGallery = true;
             } else {
@@ -225,7 +226,7 @@ export class CoachView implements OnInit {
         }
 
         if (this.galerryTab === 3) {
-            let videoContent = this.feedMediaItem.filter((data) => { return ( data.feed_media && data.feed_media[0] && data.feed_media[0].content) ?(data.feed_media[0].content.type == 'video') || (data.feed_media[0].content.type == "videoLink"):null });
+            let videoContent = this.feedMediaItem.filter((data) => { return (data.feed_media && data.feed_media[0] && data.feed_media[0].content) ? (data.feed_media[0].content.type == 'video') || (data.feed_media[0].content.type == "videoLink") : null });
             if (videoContent && videoContent.length) {
 
                 this.showVideo = true;
--- src/app/com/annaniks/marathon/pages/main/profile/profile.module.ts
+++ src/app/com/annaniks/marathon/pages/main/profile/profile.module.ts
@@ -3,11 +3,13 @@ import { SharedModule } from '../../../shared/shared.module';
 import { ProfileView } from './profile.view';
 import { ProfileRoutingModule } from './profile.routing.module';
 import { GalleryModal, ProfileCoverModal } from '../../../core/modals';
+import { Md5 } from 'ts-md5/dist/md5';
 
 @NgModule({
     declarations: [ProfileView,GalleryModal,ProfileCoverModal],
     imports: [SharedModule,ProfileRoutingModule],
-    entryComponents:[GalleryModal,ProfileCoverModal]
+    entryComponents:[GalleryModal,ProfileCoverModal],
+    providers:[Md5]
 })
 
 export class ProfileModule { }
